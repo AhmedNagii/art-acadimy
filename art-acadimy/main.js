c5a7bc8021dddd9afeb9c8ebd4676cf577ceda96
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-10-07 23:24:10 +0200

    make footer and service section responsive

--- art-acadimy/main.js
+++ art-acadimy/main.js
@@ -1,47 +1,200 @@
 
 
-function addByX(x) {
 
-   function addInputTox(input) {
-      return input + x
-   }
 
-   return addInputTox
-}
 
-/*** Uncomment these to check your work! ***/
-const addByTwo = addByX(2);
-// addByTwo(1); // => should return 3
-// addByTwo(2); // => should return 4
-// addByTwo(3); // => should return 5
 
-const addByThree = addByX(3);
-addByThree(1); // => should return 4
-addByThree(2); // => should return 5
 
-const addByFour = addByX(4);
-addByFour(4); // => should return 8
-addByFour(5); // => should return 9
 
-function once(func) {
-   let counter = 0
-   let outPut;
 
- const innerfank = (val) =>{
-   if( counter === 0 ){
-      outPut = func(val)
-      counter ++
-     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function blackjack(array) {
+
+
+
+// function dealer (num1 , num2 ){
+// let numOfInvovations = 0
+
+
+// let player1Val ;
+
+// function player1(){
+//   numOfInvovations++
+//   if(dealer === 'bust' ) return  'you are done!'
+//   if(numOfInvovations === 1) return num1 + num2
+
+// if(numOfInvovations === 2){
+//   const total = array[0] + num1 + num2
+//   if(total <= 21){
+//     return total
+//   }else{
+//     player1Val = 'bust'
+//     return player1Val
+//   }
+  
+ 
+// }
+
+// }
+
+
+
+// return player1
+
+// } 
+
+
+// return dealer
+
+// }
+
+// /*** Uncomment these to check your work! ***/
+
+// /*** DEALER ***/
+// const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+
+// /*** PLAYER 1 ***/
+// const i_like_to_live_dangerously = deal(4, 5);
+// console.log(i_like_to_live_dangerously()); // => should log 9
+// console.log(i_like_to_live_dangerously()); // => should log 11
+// console.log(i_like_to_live_dangerously()); // => should log 17
+// console.log(i_like_to_live_dangerously()); // => should log 18
+// console.log(i_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+
+// /*** BELOW LINES ARE FOR THE BONUS ***/
+
+// /*** PLAYER 2 ***/
+// const i_TOO_like_to_live_dangerously = deal(2, 2);
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+// const i_ALSO_like_to_live_dangerously = deal(3, 7);
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function makeHistory(limit) {
+//  let historyOf =[];
+
+//   return (word) => {
+//         if(word !== "undo"){
+
+//      historyOf.length < limit && historyOf.push(word) 
+//       return word + " done"
+
+//     }else if (word === "undo" && historyOf.length === 0){
     
- }
- return outPut
- }
+//       return "nothing to undo"
+      
+//     }else{
      
-    return innerfank
- }
+//       let action = historyOf[0] + " undone"
+//        historyOf.shift() 
+//       return action
+//     }
+   
+//   }
+// }
 
-   // /*** Uncomment these to check your work! ***/
-   const onceFunc = once(addByTwo);
-   console.log(onceFunc(4));  // => should log 6
-   console.log(onceFunc(10));  // => should log 6
-   console.log(onceFunc(9001));  // => should log 6
+// /*** Uncomment these to check your work! ***/
+// const myActions = makeHistory(2);
+// console.log(myActions('jump')); // => should log 'jump done'
+// console.log(myActions('undo')); // => should log 'jump undone'
+// console.log(myActions('walk')); // => should log 'walk done'
+// console.log(myActions('code')); // => should log 'code done'
+// console.log(myActions('pose')); // => should log 'pose done'
+// console.log(myActions('undo')); // => should log 'pose undone'
+// console.log(myActions('undo')); // => should log 'code undone'
+// console.log(myActions('undo')); // => should log 'nothing to undo'
+
+
+
+
+
+
+
+
+
+
+// function censor() {
+   
+//    let obj = {}
+//    function someThing (string1 , string2) {
+     
+
+//      if(string1 && string2){
+//       obj [ string1] = string2
+         
+//      }else{
+//       for(const key in obj){
+        
+//       string1 =    string1.replace(key, obj[key]);
+//       }
+//        return string1
+//      }
+     
+//    }
+   
+//  return someThing
+//  }
+ 
+//  /*** Uncomment these to check your work! ***/
+//  const changeScene = censor();
+//  changeScene('dogs', 'cats');
+//  changeScene('quick', 'slow');
+//  console.log(changeScene('The quick, brown fox jumps over the lazy dogs.'));
+ // => should log 'The slow, brown fox jumps over the lazy cats.'
