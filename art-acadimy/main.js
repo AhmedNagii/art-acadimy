a04f826a02068c615b877966e03ea596c26aba25
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-10-09 21:24:16 +0200

    collection section responsive

--- art-acadimy/main.js
+++ art-acadimy/main.js
@@ -1,77 +1,47 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // function blackjack(array) {
+//   let arrayIndex = 0;
+//   function dealer(num1, num2) {
+//     let numOfInvovations = 0;
+//     let playerVal;
 
+//     let total = array[arrayIndex] + num1 + num2;
 
-
-// function dealer (num1 , num2 ){
-// let numOfInvovations = 0
-
-
-// let player1Val ;
-
-// function player1(){
-//   numOfInvovations++
-//   if(dealer === 'bust' ) return  'you are done!'
-//   if(numOfInvovations === 1) return num1 + num2
-
-// if(numOfInvovations === 2){
-//   const total = array[0] + num1 + num2
-//   if(total <= 21){
-//     return total
-//   }else{
-//     player1Val = 'bust'
-//     return player1Val
+//     function player() {
+//       numOfInvovations++;
+//       if (numOfInvovations === 1) {
+//         return num1 + num2;
+//       } else if (numOfInvovations === 2) {
+//         if (total <= 21) {
+//           return total;
+//         } else {
+//           playerVal = "bust";
+//           return playerVal;
+//         }
+//       } else if (playerVal === "bust") {
+//         return "you are done!";
+//       } else if (numOfInvovations > 2) {
+//         arrayIndex++;
+//         // console.log("three or more: "+ numOfInvovations + " "+ "index: "+ arrayIndex + " total: "+ total)
+//         total = total + array[arrayIndex];
+//         if (total <= 21) {
+//           return total;
+//         } else {
+//           playerVal = "bust";
+//           return playerVal;
+//         }
+//       }
+//     }
+//     return player;
 //   }
-  
- 
-// }
-
-// }
-
-
-
-// return player1
-
-// } 
-
-
-// return dealer
-
+//   return dealer;
 // }
 
 // /*** Uncomment these to check your work! ***/
 
 // /*** DEALER ***/
-// const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+// const deal = blackjack([
+//   2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11,
+// ]);
 
 // /*** PLAYER 1 ***/
 // const i_like_to_live_dangerously = deal(4, 5);
@@ -102,99 +72,90 @@
 // console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
 // console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
 
+/////////////
 
+//
+// function delayCounter(target, wait) {
+//     let counter = 1
+//    return () => {
+//      if(counter < target){
+//        console.log('sssss')
+//       // const myInterval = setInterval(() => console.log(counter++ ) ,wait)
+//         }
+//    }
+//    }setTimeout
 
+//    //UNCOMMENT THESE TO TEST YOUR WORK!
+//    const countLogger = delayCounter(3, 1000)
+//    countLogger();
+//    After 1 second, log 1
+//    After 2 seconds, log 2
+//    After 3 seconds, log 3
 
+// function promised (val) {
+//   const myPromies = new Promise((resolve) => {
+//         setTimeout(()=> resolve(val), 2000)
+//   })
 
+//   return myPromies
+//   }
 
+//   //UNCOMMENT THESE TO TEST YOUR WORK!
+//   const createPromise = promised('wait for it...');
+//   createPromise.then((val) => console.log(val));
+//   //will log "wait for it..." to the console after 2 seconds
 
+// class SecondClock {
+//   constructor(cb) {
+//     this.cb = cb;
+// this.myIntern
+//   }
 
+// inter (){
+//  return 
+// }
 
+//   start() {
+//     this.myIntern =  setInterval(() => { this.cb("tic");}, 1000);
 
+//   }
 
+//   reset() {
+//     clearInterval(this.myIntern)
+//   }
+// }
+// //UNCOMMENT THESE TO TEST YOUR WORK!
+// const clock = new SecondClock((val) => {
+//   console.log(val);
+// });
+// console.log("Started Clock.");
+// clock.start();
+// setTimeout(() => {
+//   clock.reset();
+//   console.log("Stopped Clock after 6 seconds.");
+// }, 6000);
 
+// function debounce(callback, Interval) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function makeHistory(limit) {
-//  let historyOf =[];
-
-//   return (word) => {
-//         if(word !== "undo"){
-
-//      historyOf.length < limit && historyOf.push(word) 
-//       return word + " done"
-
-//     }else if (word === "undo" && historyOf.length === 0){
-    
-//       return "nothing to undo"
-      
-//     }else{
-     
-//       let action = historyOf[0] + " undone"
-//        historyOf.shift() 
-//       return action
-//     }
+//   let timeOut;
+ 
+//   return function (...args) {
+// //    console.log("prev ID : ",timeOut)
+//     if (timeOut) {clearTimeout(timeOut)} 
    
+//       timeOut = setTimeout(() => {
+//         callback(...args);
+//       }  , Interval)
 //   }
 // }
 
-// /*** Uncomment these to check your work! ***/
-// const myActions = makeHistory(2);
-// console.log(myActions('jump')); // => should log 'jump done'
-// console.log(myActions('undo')); // => should log 'jump undone'
-// console.log(myActions('walk')); // => should log 'walk done'
-// console.log(myActions('code')); // => should log 'code done'
-// console.log(myActions('pose')); // => should log 'pose done'
-// console.log(myActions('undo')); // => should log 'pose undone'
-// console.log(myActions('undo')); // => should log 'code undone'
-// console.log(myActions('undo')); // => should log 'nothing to undo'
 
 
 
-
-
-
-
-
-
-
-// function censor() {
-   
-//    let obj = {}
-//    function someThing (string1 , string2) {
-     
-
-//      if(string1 && string2){
-//       obj [ string1] = string2
-         
-//      }else{
-//       for(const key in obj){
-        
-//       string1 =    string1.replace(key, obj[key]);
-//       }
-//        return string1
-//      }
-     
-//    }
-   
-//  return someThing
-//  }
- 
-//  /*** Uncomment these to check your work! ***/
-//  const changeScene = censor();
-//  changeScene('dogs', 'cats');
-//  changeScene('quick', 'slow');
-//  console.log(changeScene('The quick, brown fox jumps over the lazy dogs.'));
- // => should log 'The slow, brown fox jumps over the lazy cats.'
+// // UNCOMMENT THESE TO TEST YOUR WORK!
+// function giveHi() { return 'Hi'  }
+// const giveHiSometimes = debounce(giveHi, 3000);
+// console.log(giveHiSometimes()); // -> 'hi'
+// setTimeout(function () { console.log(giveHiSometimes() ); }, 2000); // -> undefined
+// setTimeout(function () { console.log(giveHiSometimes() ); }, 4000); // -> undefined
+// setTimeout(function () { console.log(giveHiSometimes() ); }, 8000); // -> 'hi'
